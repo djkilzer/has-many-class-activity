5286eae3feae8e0b317db3a562fd5484ba2ecbd5
David Kilzer <kilzer15@gmail.com>
2019-02-21 15:47:29 -0600

    Correctly made tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:last_name, :first_firstname, :phone_number, :email)
+      params.require(:user).permit(:last_name, :first_name, :phone_number, :email)
     end
 end
--- db/migrate/20190221204647_create_products.rb
+++ db/migrate/20190221204647_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :product_name
       t.string :vendor_name
-      t.references :Product, foreign_key: true
+      t.references :Vendor, foreign_key: true
 
       t.timestamps
     end
